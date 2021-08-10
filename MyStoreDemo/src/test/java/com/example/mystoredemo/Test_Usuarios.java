ec57a3777b3b667d2148879de5a1de185af62091
Juliana.Sala <Juliana.Sala@tsoftglobal.com>
2021-08-10 16:50:35 -0300

    Proyecto Selenium, aplicando json para parametrizar test.
    -Loguea usuarios dentro de la URL http://automationpractice.com/index.php

---
+++ MyStoreDemo/src/test/java/com/example/mystoredemo/Test_Usuarios.java
@@ -0,0 +1,71 @@
+package com.example.mystoredemo;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+
+public class Test_Usuarios {
+    private WebDriver driver;
+    static SoftAssert softAssert = new SoftAssert();
+
+    @BeforeTest
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Juliana.Sala\\IdeaProjects\\WebDrivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://automationpractice.com/index.php");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+
+    }
+
+    @Test(dataProvider = "data")
+    public void login(String mail,String pass) {
+        WebElement webElement = driver.findElement(By.cssSelector(".login"));
+        softAssert.assertEquals(webElement.getAttribute("innerText"), "Sign in");
+        webElement.click();
+        driver.findElement(By.cssSelector("#email")).sendKeys(mail);
+        driver.findElement(By.cssSelector("#passwd")).sendKeys(pass);
+        driver.findElement(By.cssSelector("#passwd")).sendKeys(Keys.ENTER);
+        webElement = driver.findElement(By.cssSelector(".logout"));
+        softAssert.assertEquals(webElement.getAttribute("innerText"), "Sign out");
+        softAssert.assertTrue(webElement.isDisplayed(),"Usuario no logueado");
+        webElement.click();
+    }
+
+    @DataProvider (name="data")
+    public Object[][] getData() throws IOException, ParseException {
+        JSONParser jsonParser = new JSONParser();
+        FileReader reader = new FileReader(new File("data.json").getAbsolutePath());
+        //Read JSON file
+        JSONArray jsonA = (JSONArray) jsonParser.parse(reader);
+        Object[][] datos = new Object[jsonA.size()][2];
+        for (int i=0; i<jsonA.size();i++) {
+            JSONObject json =(JSONObject)jsonA.get(i);
+            JSONObject usuario = (JSONObject) json.get("usuario");
+            datos[i][0]=usuario.get("mail");
+            datos[i][1]=usuario.get("pass");
+        }
+        return datos;
+    }
+
+
+}
